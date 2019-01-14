fcdc96f0f50ebf1cc8a1f3d24f67fa8e89fcc2c9
Joey Huang <JoeyHuangOne@gmai.com>
2019-01-14 00:34:14 -0800

    test some component method

--- src/__tests__/Dialer.test.js
+++ src/__tests__/Dialer.test.js
@@ -3,31 +3,57 @@ import ReactDOM from 'react-dom';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { shallow, mount, render } from 'enzyme';
-import Dialer from '../Dialer';
+import ReduxDialer, { Dialer } from '../Dialer';
 
 import rootReducer from '../reducers'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { install } from 'redux-loop';
+import { dateChange, dateHourChange, scrollHour } from '../ActionConst.js'
 
 configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const store = createStore(rootReducer, {}, install())
   const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><Dialer /></Provider>,
+  ReactDOM.render(<Provider store={store}><ReduxDialer /></Provider>,
     div); ReactDOM.unmountComponentAtNode(div);
 });
 
-it("change date", () => {
-  const store = createStore(rootReducer,
-    { dateReducer: { currentHour: new Date('1/16/2019 06:00') } }, install())
-  let newProp = { initDate: new Date('1/16/2019 06:00') }
-  const wrapper = mount(<Provider store={store}><Dialer initDate={newProp} /></Provider>);
-  newProp = { initDate: new Date('1/16/2019 08:00') }
-  wrapper.instance().componentDidUpdate(newProp)
-  wrapper.setProps(newProp)
-  wrapper.setState({ currentHour: new Date('1/16/2019 08:00') })
-  wrapper.update();
+it("test fillHours() and convertX2Hour()", () => {
+  const wrapper = shallow(<Dialer />, { disableLifecycleMethods: true })
+  let inst = wrapper.instance()
+  inst.dialerRef = {
+    "current": {
+      "scrollLeft": 100,
+      "clientWidth": 220,
+    }
+  }
+  let newDate = new Date('1/16/2019 06:00')
+  let hours = inst.fillHours(newDate)
+  let hour0 = new Date('1/15/2019 14:00')
+  expect(hours.length).toEqual(33)
+  expect(hours[0]).toEqual(hour0)
+  expect(hours[16]).toEqual(newDate)
+  expect(hours[32]).toEqual(new Date('1/16/2019 22:00'))
+  wrapper.setState({ hours })
+
+  let newHour = inst.convertX2Hour(220)
+  expect(newHour).toEqual(newDate)
 });
 
+it("test componentDidUpdate()", () => {
+  const wrapper = shallow(<Dialer />, { disableLifecycleMethods: true })
+  let inst = wrapper.instance()
+  inst.dialerRef = {
+    "current": {
+      "scrollLeft": 100,
+      "clientWidth": 220,
+    }
+  }
+  wrapper.setProps({
+    initDate: new Date('1/16/2019 06:00'),
+    changeType: dateChange
+  })
+  inst.componentDidUpdate({ initDate: new Date('1/16/2019 09:00') })
+});
