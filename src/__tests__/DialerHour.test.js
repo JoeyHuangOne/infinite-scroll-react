fa0b3eb3ac259dc6985e185de4e6bc5a23ff9e5e
Joey Huang <JoeyHuangOne@gmai.com>
2019-01-13 23:10:09 -0800

    fix test error

--- src/__tests__/DialerHour.test.js
+++ src/__tests__/DialerHour.test.js
@@ -17,7 +17,7 @@ it('renders without crashing', () => {
   let hour = new Date('1/16/2019 06:00')
   const div = document.createElement('div');
   ReactDOM.render(<Provider store={store}><DialerHour
-    currentHour={hour} /></Provider>,
+    currentHour={hour} cellWidth='10%' /></Provider>,
     div);
   ReactDOM.unmountComponentAtNode(div);
 });
@@ -26,7 +26,7 @@ it('renders hour correctly', () => {
   const store = createStore(rootReducer, {}, install())
   let hour = new Date('1/16/2019 08:00')
   const wrapper = render(<Provider store={store}><DialerHour
-    currentHour={hour} /></Provider>);
+    currentHour={hour} cellWidth='10%' /></Provider>);
   expect(wrapper.text()).toEqual('8')
 });
 
