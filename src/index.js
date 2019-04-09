2bf5cf033bb9e2030270f3af7a5c84ba0443e7b1
Joey Huang <JoeyHuangOne@gmai.com>
2019-04-08 18:40:39 -0700

    test git fork merge

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const store = createStore(rootReducer, {}, install())
 ReactDOM.render(<Provider store={store}><App /></Provider>,
   document.getElementById('root'));
 
-
+// test for git fork merge
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
